31f6e29ed115f0047e0fc55210ef759e78795fee
JamzMarks <jamzmarks@gmail.com>
2025-02-06 03:14:30 -0300

    Review<feat> add Review json file, create function that renders a ReviewCard by given elementId and data

--- utils.js
+++ utils.js
@@ -3,3 +3,15 @@ function getFullPrice(price, percentage){
     const result = (price * 100) / perc;
     return result;
 }
+
+async function fetchData(dataUrl){
+    try {
+        const response = await fetch(dataUrl);
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar JSON: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
+    }
+}
