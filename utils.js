dc4e4436b51ec932b346c5a7b0d3dfd9c1699a2c
JamzMarks <jamzmarks@gmail.com>
2025-02-11 09:27:34 -0300

    footer<fix> Fix newsletter responsive bug

--- utils.js
+++ utils.js
@@ -45,40 +45,4 @@ function insertStarRating(obj){
     }
 }
 
-// function getStarRatings() {
-//     const starElements = document.querySelectorAll(".starRate");
-//     const ratings = [];
-
-//     starElements.forEach(div => {
-//         const firstP = div.querySelector("p");
-//         if (firstP) {
-//             const rating = parseFloat(firstP.textContent.trim(), 10);
-//             if (!isNaN(rating)) {
-//                 let ratingObj = {
-//                     element: div,
-//                     value: rating
-//                 }
-//                 ratings.push(ratingObj);
-//             }
-//         }
-//     });
-//     insertStarRating(ratings);
-// }
-
-// function insertStarRating(arr){
-//     arr.forEach(item => {
-//         const {element, value} = item
-//         const textP = element.querySelector("p");
-//         if (textP) {
-//             textP.remove();
-//         }
-//         for (let i = 0; i < Math.floor(value); i++) {
-//             const starImg = document.createElement("img");
-//             starImg.src = "images/reviews/starFull.svg";
-//             starImg.alt = "Star";
-//             starImg.classList.add("star-icon");
-//             element.appendChild(starImg);
-//         }
-//     })
-// }
 
